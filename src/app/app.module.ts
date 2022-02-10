80b9026f5161fa1defb414bf4990a0e3ea4cf688
Ephraim19 <machayoephraim@gmail.com>
2022-02-10 16:56:15 +0300

    Created an object for initializing profile data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './githubClass/user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
   ],
   imports: [
     BrowserModule,
