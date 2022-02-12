dabe6cdbf04a4cf237ae5af3841891e2cc124be1
Ephraim19 <machayoephraim@gmail.com>
2022-02-12 12:20:52 +0300

    Added a http request function to call the repos api

--- src/app/gitServices/repositories.service.ts
+++ src/app/gitServices/repositories.service.ts
@@ -1,11 +1,40 @@
 import { Injectable } from '@angular/core';
-import { User } from '../githubClass/user';
 import { HttpClient } from '@angular/common/http';
-
+import { Repos } from '../githubClass/repos';
 @Injectable({
   providedIn: 'root'
 })
 export class RepositoriesService {
+   repos!:Repos[];
+  constructor(private http:HttpClient) {
+    this.repos = [
+      new Repos("","")
+    ]
+   }
+   reposRequest() {
+    interface ApiResponse {
+      name: string;
+      url: string;
 
-  constructor() { }
+    }
+    let promise = new Promise((resolve, reject) => {
+      this.http
+        .get<ApiResponse>('https://api.github.com/users/Ephraim19/repos')
+        .toPromise()
+        .then(
+          (response: any) => {
+
+            this.repos[0] = response.name
+            this.repos[1] = response.html_url
+
+            resolve(response);
+          },
+          (error) => {
+
+            reject(error);
+          }
+        );
+    });
+    return promise;
+  }
 }
